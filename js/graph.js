e01a072288ac689e1d5eae5fc5cff07535b10fdf
Rachel Weil <Rachel Weil>
2016-12-17 04:56:19 -0600

    WIP, stopping point for December

--- js/graph.js
+++ js/graph.js
@@ -1,28 +1,73 @@
-var io = io.connect();
-var canvas = document.getElementById('c');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
 
-io.on('initData', function (incomingData) {
-    if(document.getElementById(incomingData.deviceid) !== null) {   
-        var commandFunction = new Function( "" + incomingData.deviceid + "." + incomingData.command + "");
-        commandFunction();
+var io = io.connect();
+
+
+io.on('initData', function (data) {
+
+    for (i = 0; i < data.length; i++) {
+        if (!document.getElementById(data[i].botID)) {
+            var botID = data[i].botID;
+            $('body').prepend('<div id="' + botID + '" class="turtle"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x" style="color:#0000ff"></i><i class="fa fa-' + data[i].symbol + ' fa-stack-1x" style="color:' + data[i].botColor + '"></i></span></div>');
+            $("#sidebar").append($('<p><i class="fa fa-' + data[i].symbol + ' fa-2x fa-pull-left fa-fw" style="color:' + data[i].botColor + '"></i>' + data[i].botName + ": " + data[i].totalDistance + 'ft.<BR><em>' + data[i].slogan + '</em></p>'));
         }
-    else {
-        $('<div/>', {
-            id: incomingData.deviceid,
-            class: 'turtle',
-            text: incomingData.deviceid
-        }).appendTo('body');
-        window[incomingData.deviceid] = new Turtle(canvas);
-        window[incomingData.deviceid].on('move', function(args){
-	        $('#'+incomingData.deviceid+'').css({
-		        left:args.x+'px',
-		        top:args.y+'px'
-	        });
-        });
-        window[incomingData.deviceid].init();
+        var commandFunction = new Function("t.thickness(2).color('" + data[i].botColor + "')." + data[i].currentCommand);
+        commandFunction();
+        var x = canvas.width/2 - t.get.x();
+        var y = -1*t.get.y() + canvas.height/2;
+        $('div#' + botID + '').css({
+            right: x + 'px',
+            top: y + 'px'
+        })
+        console.log(commandFunction)
+        t.pu().home().pd();
+        // var ckdistance = data.sort(function(a, b) {
+        //     return parseFloat(a.distance) - parseFloat(b.distance);
+        // });
+        // if (ckdistance > canvas.height) { canvas.scale(.85,.85) }
     }
+    io.emit('initialized');
 });
 
-io.emit('ready');
+io.on('updateData', function (data) {
+    for (i = 0; i < data.length; i++) {
+        if (!document.getElementById(data[i].botID)) {
+            $('body').prepend('<div id="' + data[i].botID + '">' + data[i].botName + '</div>');
+            $("#sidebar").append($('<p><i class="fa fa-' + data[i].symbol + ' fa-2x fa-pull-left fa-fw" style="color:' + data[i].botColor + '"></i>' + data[i].botName + ": " + data[i].totalDistance + 'ft.<BR><em>' + data[i].slogan + '</em></p>'));
+        }
+        var botID = data[i].botID;
+        var commandFunction = new Function("t.thickness(2).color('" + data[i].botColor + "')." + data[i].currentCommand);
+        commandFunction();
+        var x = canvas.width/2 - t.get.x();
+        var y = -1*t.get.y() + canvas.height/2;
+        $('div#' + botID + '').css({
+            right: x + 'px',
+            top: y + 'px'
+        })
+        console.log(commandFunction)
+        t.pu().home().pd();
+        var distance = data.sort(function(a, b) {
+            return parseFloat(a.distance) - parseFloat(b.distance);
+        });
+        if (data[data.length].distance > canvas.height) { canvas.scale(.85,.85) }
+    }
+
+
+    //         color.push(color.shift());
+    //         $("table").find('tbody').append($('<tr>').append($('<td>').text('' + data[i].RowKey._ + ": " + window[deviceobj].distance)));
+
+
+});
+
+var clearit;
+
+$(window).resize(function () {
+    $( 'body' ).hide(); 
+    clearTimeout(clearit);
+    clearit = setTimeout(resizewait, 500);
+});
+
+function resizewait(){
+    location.reload();
+    $( 'body' ).show();
+}
+
--- server.js
+++ server.js
@@ -1,13 +1,117 @@
 // Initialize modules
 var azure = require('azure-storage');
 var port = process.env.PORT || 3000;
-var express = require('express'); 
-var app = express(); 
-var server = require('http').createServer(app); 
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var tableService = azure.createTableService();
 var entGen = azure.TableUtilities.entityGenerator;
+var botDirectory = [
+];
+var lastUpdate = 0;
+var color = [
+    '#FFFF00', // Bright yellow
+    '#00FF00', // Bright green
+    '#00FFFF', // Bright cyan
+    '#FF00FF', // Bright pink
+    '#FF0000', // Bright red
+    '#AA00FF', // Bright purple
+    '#FFAA00', // Bright orange
+    '#00FFAA', // Bright blue-green
 
+    '#FFFF55', // Light yellow
+    '#55FF55', // Light green
+    '#55FFFF', // Light cyan
+    '#FF55FF', // Light pink
+    '#FF5555', // Light red
+    '#AA55FF', // Light purple
+    '#FFAA55', // Light orange
+    '#55FFAA', // Light blue-green
+    ]
+var symbols = [
+    {   slogan: "speedDemon.js.",
+        icon: "dashboard"
+    },
+    {   slogan: "Perpetually lost.",
+        icon: "map-marker"
+    },
+    {   slogan: "Rebel without a cause.",
+        icon: "rebel"
+    },
+    {   slogan: "Powered by ESP8266.",
+        icon: "microchip"
+    },
+    {   slogan: "Was it supposed to do that?!",
+        icon: "exclamation-triangle"
+    },
+    {   slogan: "Headed toward the finish line.",
+        icon: "flag-checkered"
+    },
+    {   slogan: "Hang in there, baby.",
+        icon: "heartbeat"
+    },
+    {   slogan: "Really, really, really fast.",
+        icon: "fast-forward"
+    },
+    {   slogan: "Basically priceless.",
+        icon: "diamond"
+    },
+    {   slogan: "Fast as lightning!",
+        icon: "bolt"
+    },
+    {   slogan: "Eye on the prize.",
+        icon: "eye"
+    },
+    {   slogan: "????????",
+        icon: "question-circle"
+    },
+    {   slogan: "Pow-pow-power wheels!",
+        icon: "power-off"
+    },
+    {   slogan: "Uniquely unique.",
+        icon: "snowflake-o"
+    },
+    {   slogan: "Toot, toot! Ahh beep, beep!",
+        icon: "bus"
+    },
+    {   slogan: "A bit of a fixer-upper.",
+        icon: "wrench"
+    },
+    {   slogan: "IoT? Oh my!",
+        icon: "wifi"
+    },
+    {   slogan: "It's not a bug, it's a feature.",
+        icon: "bug"
+    },
+    {   slogan: "Um, no.",
+        icon: "ban"
+    },
+    {   slogan: "It's aliiive!",
+        icon: "cogs"
+    },
+    {   slogan: "Send help.",
+        icon: "life-buoy"
+    },
+    {   slogan: "A bot powered by magic. (And Javascript.)",
+        icon: "magic"
+    },
+    {   slogan: "LUV2CODE",
+        icon: "heart"
+    },
+    {   slogan: "It's not broken. It's... experimental.",
+        icon: "flask"
+    },
+    {   slogan: "Oh, dear.",
+        icon: "frown-o"
+    },
+    {   slogan: "Going places.",
+        icon: "line-chart"
+    },
+    {   slogan: "On fire! (Metaphorically, right?)",
+        icon: "fire-extinguisher"
+    }
+]
 // Initialize web app
 server.listen(port);
 app.use(express.static(__dirname));
@@ -16,13 +120,12 @@ console.log("App started.");
 // Populate webpage with all existing table data
 io.on('connection', function (socket) {
     initTable();
-    
-    // Every 10 seconds, refresh page with new table data only
-    var lastUpdate;
 
-    socket.on('initialized', function() {
+    // Every 10 seconds, refresh page with new table data only
+    socket.on('initialized', function () {
         setInterval(updateTable, 10000);
     });
+
     // Upon disconnection, stop refreshing the table
     socket.on('disconnect', function () {
         clearInterval(setInterval(updateTable, 10000));
@@ -30,32 +133,195 @@ io.on('connection', function (socket) {
 
     // Function: Fetch table data on page load
     function initTable() {
-        var initQuery = new azure.TableQuery().select(['PartitionKey', 'RowKey', 'command', 'distance']);
+        var color = [
+    '#FFFF00', // Bright yellow
+    '#00FF00', // Bright green
+    '#00FFFF', // Bright cyan
+    '#FF00FF', // Bright pink
+    '#FF0000', // Bright red
+    '#AA00FF', // Bright purple
+    '#FFAA00', // Bright orange
+    '#00FFAA', // Bright blue-green
+
+    '#FFFF55', // Light yellow
+    '#55FF55', // Light green
+    '#55FFFF', // Light cyan
+    '#FF55FF', // Light pink
+    '#FF5555', // Light red
+    '#AA55FF', // Light purple
+    '#FFAA55', // Light orange
+    '#55FFAA', // Light blue-green
+    ]
+    var symbols = [
+    {   slogan: "speedDemon.js.",
+        icon: "dashboard"
+    },
+    {   slogan: "Perpetually lost.",
+        icon: "map-marker"
+    },
+    {   slogan: "Rebel without a cause.",
+        icon: "rebel"
+    },
+    {   slogan: "Powered by ESP8266.",
+        icon: "microchip"
+    },
+    {   slogan: "Was it supposed to do that?!",
+        icon: "exclamation-triangle"
+    },
+    {   slogan: "Headed toward the finish line.",
+        icon: "flag-checkered"
+    },
+    {   slogan: "Hang in there, baby.",
+        icon: "heartbeat"
+    },
+    {   slogan: "Really, really, really fast.",
+        icon: "fast-forward"
+    },
+    {   slogan: "Basically priceless.",
+        icon: "diamond"
+    },
+    {   slogan: "Fast as lightning!",
+        icon: "bolt"
+    },
+    {   slogan: "Eye on the prize.",
+        icon: "eye"
+    },
+    {   slogan: "????????",
+        icon: "question-circle"
+    },
+    {   slogan: "Pow-pow-power wheels!",
+        icon: "power-off"
+    },
+    {   slogan: "Uniquely unique.",
+        icon: "snowflake-o"
+    },
+    {   slogan: "Toot, toot! Ahh beep, beep!",
+        icon: "bus"
+    },
+    {   slogan: "A bit of a fixer-upper.",
+        icon: "wrench"
+    },
+    {   slogan: "IoT? Oh my!",
+        icon: "wifi"
+    },
+    {   slogan: "It's not a bug, it's a feature.",
+        icon: "bug"
+    },
+    {   slogan: "Um, no.",
+        icon: "ban"
+    },
+    {   slogan: "It's aliiive!",
+        icon: "cogs"
+    },
+    {   slogan: "Send help.",
+        icon: "life-buoy"
+    },
+    {   slogan: "A bot powered by magic. (And Javascript.)",
+        icon: "magic"
+    },
+    {   slogan: "LUV2CODE",
+        icon: "heart"
+    },
+    {   slogan: "It's not broken. It's... experimental.",
+        icon: "flask"
+    },
+    {   slogan: "Oh, dear.",
+        icon: "frown-o"
+    },
+    {   slogan: "Going places.",
+        icon: "line-chart"
+    },
+    {   slogan: "On fire! (Metaphorically, right?)",
+        icon: "fire-extinguisher"
+    }
+]
+        botDirectory = [];
+        var initQuery = new azure.TableQuery().select(['PartitionKey', 'RowKey', 'command', 'distance'])
+        //.where('PartitionKey gt ?', lastUpdate);
         tableService.queryEntities('outTable', initQuery, null, function (error, result, response) {
             if (!error) {
-                lastUpdate = result.entries[result.entries.length - 1].PartitionKey._
-                socket.emit('initData', result.entries);
-                console.log('Updates with entries sent up to ' + lastUpdate);
+
+                // For each row of table, check to see if bot is listed in bot directory
+                for (i = 0; i < result.entries.length; i++) {
+
+                    function checkExist(botname) { return botname.botName == result.entries[i].RowKey._; }
+
+                    var botIndex = botDirectory.findIndex(checkExist);
+
+                    // If bot is already in bot directory, update content
+                    if (botIndex >= 0) {
+                        botDirectory[botIndex].currentCommand = "" + botDirectory[botIndex].currentCommand + "." + result.entries[i].command._ + "";
+                        botDirectory[botIndex].totalDistance = botDirectory[botIndex].totalDistance + parseInt(result.entries[i].distance._);
+                    }
+
+                    // If bot is not in bot directory, add an entry for it
+                    else {
+                        botDirectory.push(
+                            {
+                                botName: result.entries[i].RowKey._,
+                                botID: "bot" + result.entries[i].PartitionKey._,
+                                currentCommand: result.entries[i].command._,
+                                totalDistance: parseInt(result.entries[i].distance._),
+                                botColor: color[0],
+                                slogan: symbols[0].slogan,
+                                symbol: symbols[0].icon
+                            }
+                        );
+                        color.push(color.shift());
+                        symbols.push(symbols.shift());
+                    }
+                }
+                lastUpdate = result.entries[result.entries.length - 1].PartitionKey._;
+                socket.emit('initData', botDirectory);
+                console.log('Initial entries sent up to ' + lastUpdate);
+                console.log(botDirectory);
             }
         });
     }
 
-    // Function: Fetch new table data every 10 seconds
+    // Function: Fetch table data every 10 seconds
     function updateTable() {
-        var updateQuery = new azure.TableQuery()
-            .select(['PartitionKey', 'RowKey', 'command'])
+        var updateQuery = new azure.TableQuery().select(['PartitionKey', 'RowKey', 'command', 'distance'])
             .where('PartitionKey gt ?', lastUpdate);
         tableService.queryEntities('outTable', updateQuery, null, function (error, result, response) {
             if (!error) {
-                if (result.entries.length > 0){
-                lastUpdate = result.entries[result.entries.length - 1].PartitionKey._
-                socket.emit('updateData', result.entries);
-                console.log('Updates with entries sent up to ' + lastUpdate);
+                if (result.entries.length > 0) {
+
+                    // For each row of table, check to see if bot is listed in bot directory
+                    for (i = 0; i < result.entries.length; i++) {
+
+                        function checkExist(botname) { return botname.botName == result.entries[i].RowKey._; }
+
+                        var botIndex = botDirectory.findIndex(checkExist);
+
+                        // If bot is already in bot directory, update content
+                        if (botIndex >= 0) {
+                            botDirectory[botIndex].currentCommand = "" + botDirectory[botIndex].currentCommand + "." + result.entries[i].command._ + "";
+                            botDirectory[botIndex].totalDistance = parseInt(botDirectory[botIndex].totalDistance) + parseInt(result.entries[i].distance._);
+                        }
+                        else {
+                            botDirectory.push(
+                                {
+                                    botName: result.entries[i].RowKey._,
+                                    botID: "bot" + result.entries[i].PartitionKey._,
+                                    currentCommand: result.entries[i].command._,
+                                    totalDistance: parseInt(result.entries[i].distance._),
+                                    botColor: color[0],
+                                    slogan: symbols[0].slogan,
+                                    symbol: symbols[0].icon
+                                }
+                            );
+                            color.push(color.shift());
+                            symbols.push(symbols.shift());
+                        }
+                    }
+                    lastUpdate = result.entries[result.entries.length - 1].PartitionKey._;
+                    socket.emit('updateData', botDirectory);
+                    console.log('Updates with entries sent up to ' + lastUpdate);
+                    console.log(botDirectory);
                 }
-            else {
-                console.log('No updates at this time.');
             }
-            }
+            console.log("No updates.");
         });
     }
 });
