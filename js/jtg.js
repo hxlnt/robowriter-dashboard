3d6fc7d3c7f4c10df6dedc56b4b1bce1508b584e
Rachel Weil <rae@Rachels-iMac.local>
2017-01-05 04:26:17 -0600

    decrease scaling

--- js/jtg.js
+++ js/jtg.js
@@ -46,8 +46,8 @@ window.Turtle = function(canvas){
 	var canvasWidth = canvas.width;
 	var canvasHeight = canvas.height;
 	ctx.lineCap = 'round';
-	ctx.scale(.55,.55);
-	ctx.translate((canvas.width * .55), (canvas.height * .55 ))
+	ctx.scale(.75,.75);
+	ctx.translate((canvas.width * .75), (canvas.height * .75 ))
 
 	var T = this;
 
